fb10d7c7bd03d3ef12a84053797b7c8d2a0af41a
Ryan Myrvold <2127198+RyanMyrvold@users.noreply.github.com>
2025-04-13 22:47:16 -0400

    Reconfigure Jest Config.

--- jest.config.browser.ts
+++ jest.config.browser.ts
@@ -1,14 +1,14 @@
 import baseConfig from './jest.config.base';
-import type { Config } from '@jest/types';
+import type { Config } from 'jest';
 
-const config: Config.InitialOptions = {
+const config: Config = {
   ...baseConfig,
   displayName: 'browser',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/packages/browser/tests/**/*.(test|spec).ts'],
   globals: {
     'ts-jest': {
-      ...baseConfig.globals?.['ts-jest'],
+      ...(baseConfig.globals?.['ts-jest'] as Record<string, unknown>),
       tsconfig: '<rootDir>/packages/browser/tsconfig.browser.json'
     }
   }
